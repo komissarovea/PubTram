46e90ffe49034d7fb8c13dc353629cf58fc476f1
Komissarov Eugene <komissarovea@gmail.com>
2016-12-26 22:52:24 +0300

    Add splash activity and dependent resources.

---
+++ app/src/main/java/ru/komissarovea/pubtram/SplashActivity.java
@@ -0,0 +1,51 @@
+package ru.komissarovea.pubtram;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new PrefetchData().execute();
+    }
+
+    /*
+	 * Async Task
+	 */
+    private class PrefetchData extends AsyncTask<Void, Void, Void> {
+
+        //private ArrayList<Stop> stops = null;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            try {
+                Thread.sleep(3000);
+                //DatabaseHelper dbHelper = new DatabaseHelper(SplashActivity.this);
+                //ArrayList<Stop> stops = dbHelper.getAllStops();
+                //StopsHelper.setActualStops(stops);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            // i.putExtra("stops", stops);
+            startActivity(i);
+
+            // close this activity
+            finish();
+        }
+    }
+}
