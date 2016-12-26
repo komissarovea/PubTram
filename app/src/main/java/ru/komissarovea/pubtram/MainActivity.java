6a4ef37f160fe78e384c54acc57e0da84ab17f7a
Komissarov Eugene <komissarovea@gmail.com>
2016-12-26 20:52:35 +0300

    Initial commit

---
+++ app/src/main/java/ru/komissarovea/pubtram/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.komissarovea.pubtram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
