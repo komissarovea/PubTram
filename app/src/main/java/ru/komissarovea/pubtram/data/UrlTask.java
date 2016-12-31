23282427f810bb74ca10cd1543dbc34123642a74
Komissarov Eugene <komissarovea@gmail.com>
2016-12-31 17:54:07 +0300

    Add htmlcleaner lib.

--- app/src/main/java/ru/komissarovea/pubtram/data/UrlTask.java
+++ app/src/main/java/ru/komissarovea/pubtram/data/UrlTask.java
@@ -4,16 +4,18 @@ import android.os.AsyncTask;
 import android.view.View;
 import android.widget.ProgressBar;
 
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.TagNode;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class UrlTask extends AsyncTask<String, Void, ArrayList<Transport>> {
 
-    private String title;
     private ProgressBar mProgressBar;
     private OnRequestComplete mRequestComplete;
 
@@ -36,39 +38,74 @@ public class UrlTask extends AsyncTask<String, Void, ArrayList<Transport>> {
 
     @Override
     protected ArrayList<Transport> doInBackground(String... params) {
-        ArrayList<Transport> list = new ArrayList<Transport>();
-        try {
-            Document doc = null;//Здесь хранится будет разобранный html документ
-            try {
-                doc = Jsoup.connect(params[0]).timeout(60000).get();
-                Elements tdElements = doc.select("td");
-                if (tdElements != null && tdElements.size() > 0) {
-                    Transport trans = null;
-                    for (int i = 0; i < tdElements.size(); i++) {
-                        int rest = i % 3;
-                        if (rest == 0) {
-                            trans = new Transport();
-                            trans.setType(tdElements.get(i).text());
-                        } else if (rest == 1) {
-                            Elements linkElements = tdElements.get(i)
-                                    .select("a");
-                            if (linkElements.size() > 0)
-                                trans.setNumber(linkElements.get(0).text());
-                        } else if (rest == 2) {
-                            trans.setTime(tdElements.get(i).text());
+        return getListByHtmlCleaner(params[0]);
+    }
 
-//                            if (!TextUtils.isEmpty(trans.getNumber())
-//                                    && !TextUtils.isEmpty(trans.getNumber())
-//                                    && !TextUtils.isEmpty(trans.getNumber()))
-                                list.add(trans);
-                        }
+    private ArrayList<Transport> getListByJsoup(String url) {
+        ArrayList<Transport> list = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(url).timeout(60000).get();
+            Elements tdElements = doc.select("td");
+            if (tdElements != null && tdElements.size() > 0) {
+                Transport trans = null;
+                for (int i = 0; i < tdElements.size(); i++) {
+                    int rest = i % 3;
+                    if (rest == 0) {
+                        trans = new Transport();
+                        trans.setType(tdElements.get(i).text());
+                    } else if (rest == 1) {
+                        Elements linkElements = tdElements.get(i)
+                                .select("a");
+                        if (linkElements.size() > 0)
+                            trans.setNumber(linkElements.get(0).text());
+                    } else if (rest == 2) {
+                        trans.setTime(tdElements.get(i).text());
+
+                        list.add(trans);
                     }
                 }
-                //doc = Jsoup.connect("http://harrix.org").get();
-            } catch (IOException e) {
-                //Если не получилось считать
-                e.printStackTrace();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private ArrayList<Transport> getListByHtmlCleaner(String url) {
+        ArrayList<Transport> list = new ArrayList<>();
+        try {
+            HtmlCleaner cleaner = new HtmlCleaner();
+            TagNode rootNode = cleaner.clean(new URL(url));
+            TagNode tdElements[] = rootNode.getElementsByName("td", true);
+            if (tdElements != null && tdElements.length > 0) {
+                Transport trans = null;
+                for (int i = 0; i < tdElements.length; i++) {
+                    int rest = i % 3;
+                    if (rest == 0) {
+                        trans = new Transport();
+                        trans.setType(tdElements[i].getText().toString());
+                    } else if (rest == 1) {
+                        TagNode linkElements[] = tdElements[i]
+                                .getElementsByName("a", true);
+                        if (linkElements.length > 0)
+                            trans.setNumber(linkElements[0].getText()
+                                    .toString());
+                    } else if (rest == 2) {
+                        trans.setTime(tdElements[i].getText().toString());
+
+//                        if (!TextUtils.isEmpty(trans.getNumber())
+//                                && !TextUtils.isEmpty(trans.getNumber())
+//                                && !TextUtils.isEmpty(trans.getNumber()))
+                            list.add(trans);
+                    }
+                }
+            }
+            // RestServiceProxy serviceProxy = new RestServiceProxy();
+            // HttpResponse response = serviceProxy.webGet(params[0]);
+            // HttpEntity entity = response.getEntity();
+            // String charset = "utf-8";//
+            // EntityUtils.getContentCharSet(entity);
+            // res = EntityUtils.toString(entity, charset);
 
         } catch (Exception e) {
             e.printStackTrace();
